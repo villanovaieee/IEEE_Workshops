30e614d1de06bf7f698158249d2569762b4708d3
griffindavis02 <davisgriffinp@gmail.com>
2022-02-07 18:20:14 -0500

    python working

--- Web_Scraping/Travel/README.md
+++ Web_Scraping/Travel/README.md
@@ -1,2 +1,14 @@
 Download the chrome driver from:
 https://chromedriver.chromium.org/downloads
+
+Download python:
+https://www.python.org/downloads/
+
+Download nodejs:
+https://nodejs.org/en/download/
+
+But I recommend downloading NVM to use for nodejs:
+MAC/Linux: https://github.com/nvm-sh/nvm#installing-and-updating
+Windows: https://github.com/coreybutler/nvm-windows/releases
+
+for nvm-windows, use the nvm-setup.zip file
--- Web_Scraping/email_python.py
+++ Web_Scraping/email_python.py
@@ -3,31 +3,40 @@ import smtplib
 from email.message import EmailMessage
 from dotenv import load_dotenv
 
+
 def getAccount(dotenv_path=''):
     """
     Get the environment variables `GM_USERNAME` and `GM_PASSWORD` from the
     specified `.env` file and return as a tuple.
-    
+
     example:
     `user, password = getAccount('./.env')`
     """
-    if dotenv_path: load_dotenv(dotenv_path=dotenv_path)
-    else: load_dotenv()
+    if dotenv_path:
+        load_dotenv(dotenv_path=dotenv_path)
+    else:
+        load_dotenv()
     return (os.getenv('GM_USERNAME'), os.getenv('GM_PASSWORD'))
 
 
-def emailUpdate(subject,
-                from_email,
-                to_email,
-                password,
-                htmlText='',
-                text=''):
+def emailUpdate(subject, from_email, to_email, password, text, htmlText=''):
     """
-    Email data collected in the main function
+    Email a message with the provided account details and message.
+
     Parameters
     ----------
-    data : dict[]
-        List of dictionaries with title, link, and price of wishlist items
+    subject : str
+        Email subject
+    from_email : str
+        Account to sign into
+    to_email : str, list[str]
+        Email recipient(s)
+    password : str
+        Password for account `from_email`
+    text : str
+        Plain text message to be sent
+    htmlText : str
+        Html alternative message for rich text formatting
     """
     msg = EmailMessage()
     msg['Subject'] = subject
@@ -35,7 +44,8 @@ def emailUpdate(subject,
     msg['To'] = to_email
 
     msg.set_content(text)
-    msg.add_alternative(htmlText, subtype='html')
+    if htmlText:
+        msg.add_alternative(htmlText, subtype='html')
 
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
         smtp.login(from_email, password)
